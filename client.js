9b097f9da1107f2e246f273e72cb836d40b1cf0a
Rohan Janjua <rohan.janjua@gmail.com>
2016-12-20 11:34:13 +0000

    gridManagerUrl in client is configurable

--- client.js
+++ client.js
@@ -1,13 +1,15 @@
 const request = require('superagent');
 
-const Client = function() {};
+const Client = function(gridManagerUrl) {
+    this.gridManagerUrl = gridManagerUrl;
+};
 
 var startCount = 0;
 var closeCount = 0;
 
 Client.prototype.startNewSession = function(){
     return new Promise((resolve, reject) => { 
-        request.post('http://localhost:9876/new')
+        request.post(this.gridManagerUrl + '/new')
         .end( (err, res) => {
             startCount = startCount + 1;
             resolve(res.body);
@@ -18,7 +20,7 @@ Client.prototype.startNewSession = function(){
 
 Client.prototype.getSession = function() {
     return new Promise((resolve, reject) => { 
-        request.get('http://localhost:9876/get')
+        request.get(this.gridManagerUrl +  '/get')
         .end( (err, res) => {
             resolve(res.body);
         });
@@ -27,7 +29,7 @@ Client.prototype.getSession = function() {
 
 Client.prototype.releaseSession = function(sesh){
     return new Promise((resolve, reject) => { 
-        request.post('http://localhost:9876/release/' + sesh)
+        request.post(this.gridManagerUrl + '/release/' + sesh)
         .end( (err, res) => {
             resolve();
         });
@@ -36,7 +38,7 @@ Client.prototype.releaseSession = function(sesh){
 
 Client.prototype.closeSession = function(sesh){
     return new Promise((resolve, reject) => {
-        request.post('http://localhost:9876/close/' + sesh.id)
+        request.post(this.gridManagerUrl + '/close/' + sesh.id)
         .end( (err, res) => {
             closeCount = closeCount + 1;
             resolve({closed: true});
